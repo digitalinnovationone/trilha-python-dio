48a497b11152e568c8eadc6cd0ec31fae9d56eaa
Guilherme Carvalho <gui.ifsp11@gmail.com>
2022-09-06 04:48:16 -0300

    feat: add oop module files

---
+++ 02 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
@@ -0,0 +1,20 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+        idade = 2022 - ano
+        return cls(nome, idade)
+
+    @staticmethod
+    def e_maior_idade(idade):
+        return idade >= 18
+
+
+p = Pessoa.criar_de_data_nascimento(1994, 3, 21, "Guilherme")
+print(p.nome, p.idade)
+
+print(Pessoa.e_maior_idade(18))
+print(Pessoa.e_maior_idade(8))
