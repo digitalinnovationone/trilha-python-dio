10d1b19a24276ccf3e0c9710e4c1dce448379d82
unknown <hudsantos15@gmail.com>
2023-07-14 02:07:52 -0300

    Atualização

--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -6,6 +6,7 @@ def exibir_poema(data_extenso, *args, **kwargs):
 
 
 exibir_poema(
+    "Quinta-feira, 13 de Julho de 2023"
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
