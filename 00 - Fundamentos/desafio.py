34bf41fede0909ab1a076c9d7ce6cf33b3e9951e
sergio <sergioalmeidaaraujo@gmail.com>
2023-07-17 21:25:44 -0300

    "implentacao de limite diario"

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -1,3 +1,4 @@
+import  datetime
 menu = """
 
 [d] Depositar
@@ -10,12 +11,13 @@ menu = """
 saldo = 0
 limite = 500
 extrato = ""
-numero_saques = 0
+numero_saques_no_dia = 0
 LIMITE_SAQUES = 3
-
+data_ultimo_saque = datetime.date.today()
+print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 while True:
-
-    opcao = input(menu)
+    print(f"Saldo atual: {saldo:.2f}")
+    opcao = input(menu.lower())
 
     if opcao == "d":
         valor = float(input("Informe o valor do depósito: "))
@@ -34,7 +36,7 @@ while True:
 
         excedeu_limite = valor > limite
 
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        excedeu_saques = numero_saques_no_dia >= LIMITE_SAQUES
 
         if excedeu_saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
@@ -48,8 +50,18 @@ while True:
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
+            if data_ultimo_saque != datetime.date.today():
+                data_ultimo_saque = datetime.date.today()
+                numero_saques_no_dia = 1
+            else: numero_saques_no_dia += 1
+            linha = '='
 
+            titulo = 'SAQUE'
+            print("\n", titulo.center(50, '='))
+            print("Saque realizado com sucesso!")
+            print(f"\nSaldo: R$ {saldo:.2f}")
+            print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+            print(50 * '=')
         else:
             print("Operação falhou! O valor informado é inválido.")
 
