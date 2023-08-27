a0d018da5311b8a62eaa91bd37f6e47631ff83a4
Élico Júnior <elicogjunior@gmail.com>
2023-08-27 14:03:35 -0400

    Docs: Update Copy

--- 01 - Estrutura de dados/01 - Listas/09_copy.py
+++ 01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -1,5 +1,12 @@
 lista = [1, "Python", [40, 30, 20]]
 
-lista.copy()
+l2 = lista.copy()
 
 print(lista)  # [1, "Python", [40, 30, 20]]
+
+print(id(l2), id(lista))
+
+l2[0] = 2
+
+print(l2)
+print(lista)
