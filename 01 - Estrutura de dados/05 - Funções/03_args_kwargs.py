1320e99ef7549197f6d0fb892cfb06abd85952ff
Élico Júnior <elicogjunior@gmail.com>
2023-08-27 19:16:36 -0400

    Update: Data por Extenso

--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -6,6 +6,7 @@ def exibir_poema(data_extenso, *args, **kwargs):
 
 
 exibir_poema(
+    "Domingo, 27 de Agosto de 2023",
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
