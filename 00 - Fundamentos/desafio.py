b3bec5edcf221ddf60cbb1fa49e6789818478c47
Fabíola <161979940+ordemparcial@users.noreply.github.com>
2024-04-21 20:21:19 -0300

    Update desafio.py

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -13,14 +13,15 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-def depositar(valor):
+def depositar(saldo, valor, extrato):
     if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
     else:
             print("Operação falhou! O valor informado é inválido.")
+    return saldo, extrato
 
-def sacar(valor):
+def sacar(saldo, valor, extrato):
     if valor > saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
 
@@ -34,8 +35,9 @@ def sacar(valor):
             numero_saques += 1
     else:
             print("Operação falhou! O valor informado é inválido.")
+    return saldo, extrato
 
-def gerar_extrato():
+def gerar_extrato(extrato):
         print("\n================ EXTRATO ================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
@@ -47,14 +49,14 @@ while True:
 
     if opcao == "d":
         valor = float(input("Informe o valor do depósito: "))
-        depositar(valor)
+        depositar(saldo, valor, extrato)
 
     elif opcao == "s":
         valor = float(input("Informe o valor do saque: "))
-        sacar(valor)
+        sacar(saldo, valor, extrato)
 
     elif opcao == "e":
-        gerar_extrato()
+        gerar_extrato(extrato)
 
     elif opcao == "q":
         break
