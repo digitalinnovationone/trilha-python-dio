4d1b62116672fe9932bab4788735d6de8112a3e4
branca <cristinarenata038@gmail.com>
2024-04-29 21:10:09 -0300

    adicionada a funcionalidade de transferência e inclusão da data e hora

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -1,9 +1,10 @@
 menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Transferência
+[5] Sair
 
 => """
 
@@ -11,13 +12,13 @@ saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
-LIMITE_SAQUES = 3
+LIMITE_SAQUES = 5 
 
 while True:
 
     opcao = input(menu)
 
-    if opcao == "d":
+    if opcao == "1":
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
@@ -27,7 +28,7 @@ while True:
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "s":
+    elif opcao == "2":
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
@@ -53,13 +54,35 @@ while True:
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "e":
-        print("\n================ EXTRATO ================")
+    elif opcao == "3":
+        print("\n--------------- EXTRATO ---------------")
+
+        from datetime import datetime 
+        data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        print(f"Data e Hora: {data_hora}")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
+        print("----------------------------------------")
+ 
 
-    elif opcao == "q":
+    elif opcao == "4":
+        valor = float(input("Informe o valor da transferência: "))
+        conta_destino = input("Informe o número da conta de destino: ")
+
+        if valor > 0:
+         if valor <= saldo: 
+             saldo -= valor 
+             extrato += f"Transferência para {conta_destino}: R${valor:.2f}\n"
+             print("Transferência realizada com sucesso!")
+         else:
+             print("Operação falhou! Você não tem saldo suficiente para realizar a transferência. ")
+        else:
+            print("Operação falhou! Valor informado é inválido")
+
+    
+
+    elif opcao == "5":
         break
 
     else:
