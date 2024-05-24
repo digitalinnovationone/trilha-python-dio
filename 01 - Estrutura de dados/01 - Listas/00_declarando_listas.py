ae15c2d76fd66ce1c56fcb0f722dfa252e8ae57d
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-05-24 14:15:30 +0000

    linhas adicionadas

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -1,14 +1,19 @@
+print("\nExemplo 1 ")
 frutas = ["laranja", "maca", "uva"]
 print(frutas)
 
+print("\nExemplo 2 ")
 frutas = []
 print(frutas)
 
+print("\nExemplo 3 ")
 letras = list("python")
 print(letras)
 
+print("\nExemplo 4 ")
 numeros = list(range(10))
 print(numeros)
 
+print("\nExemplo 5 ")
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
