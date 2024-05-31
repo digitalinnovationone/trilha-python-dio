bc162e23114906b81f0cb221cdfcd3836df32015
edusousa78 <sousa.ej@gmail.com>
2024-05-31 06:23:34 -0300

    update

--- 05 - Manipulação de arquivos/1_operacao_leitura.py
+++ 05 - Manipulação de arquivos/1_operacao_leitura.py
@@ -1,25 +1,22 @@
 # Lembre-se de alterar o caminho do arquivo, para o caminho completo da sua máquina!
 
-arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/lorem.txt", "r"
+arquivo = open("05 - Manipulação de arquivos\lorem.txt", "r"
 )
 print(arquivo.read())
 arquivo.close()
 
 arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/lorem.txt", "r"
+    "05 - Manipulação de arquivos/lorem.txt", "r"
 )
 print(arquivo.readline())
 arquivo.close()
 
-arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/lorem.txt", "r"
+arquivo = open( "05 - Manipulação de arquivos/lorem.txt", "r"
 )
 print(arquivo.readlines())
 arquivo.close()
 
-arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/lorem.txt", "r"
+arquivo = open("05 - Manipulação de arquivos/lorem.txt", "r"
 )
 # tip
 while len(linha := arquivo.readline()):
