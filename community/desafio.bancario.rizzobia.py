e8d184f3856bde69d67ecd64db34d1cd83f63f89
Beatriz Rizzo <coding.bea@gmail.com>
2024-06-30 18:47:14 -0300

    feat: add RizzoBia profile

---
+++ community/desafio.bancario.rizzobia.py
@@ -0,0 +1,70 @@
+menu = """
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+=> """
+
+saldo = 0
+valor_limite_saque = 500
+extrato = ""
+numero_saques= 0
+numero_limite_saques = 3
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Valor do depósito: R$ "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > valor_limite_saque
+
+        excedeu_saques = numero_saques >= numero_limite_saques
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "0":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
+
+
+
